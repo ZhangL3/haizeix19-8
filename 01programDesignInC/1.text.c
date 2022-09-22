f25035a903ade0e821ae689a93fa2cfc02551c1a
L3 Zhang <zhanglebielefeld@gmail.com>
2022-09-22 22:17:17 +0200

    printf & scanf

---
+++ 01programDesignInC/1.text.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+	int n;
+	scanf("%d", &n);
+	printf(" has %d digits\n", printf("%d", n));
+
+	return 0;
+}
+
+/* 
+234
+output 234 has 3 digits
+
+234 print from second printf
+3 is the return from second printf
+ */
---
+++ 01programDesignInC/2.text.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+	// str: 100 位长的字符串
+	char str[100];
+	scanf("%[^\n]s", str);
+	printf(" has %d chars\n", printf("%s", str));
+	
+	return 0;
+}
