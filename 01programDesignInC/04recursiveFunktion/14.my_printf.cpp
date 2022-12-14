af92d9fbc409fe5c1c160f3760c0774c78052077
L3 Zhang <zhanglebielefeld@gmail.com>
2022-12-14 09:36:55 +0100

    my_printf finish

--- 01programDesignInC/04recursiveFunktion/14.my_printf.cpp
+++ 01programDesignInC/04recursiveFunktion/14.my_printf.cpp
@@ -1,16 +1,38 @@
 #include <cstdarg>
 #include <stdio.h>
 #include <stdarg.h>
-// 定义语句的宏
-#define PUTC(a) putchar(a), ++cnt
+#include <inttypes.h>
+
+int reverse_num(int num ,int *temp) {
+  // 123 => 321, 但 1000 => 0001 => 1
+  // 如果用 while，x 是 0 的时候无法进入，所以用 do while
+  int digit = 0;
+  do {
+    *temp = *temp * 10 + num % 10;
+    num /= 10;
+    digit += 1;
+  } while(num);
+  return digit;
+}
+
+int output_num(int temp, int digit) {
+  int cnt = 0;
+  while (digit--) {
+    putchar(temp % 10 + '0');
+    temp /= 10;
+    cnt += 1;
+  }
+  return cnt;
+}
 
 // 返回在标准输出的字符的个数
 // const 可以接收字符串的字面量
 int my_printf(const char *frm, ...) {
   int cnt = 0;
+  // 定义语句的宏
+  #define PUTC(a) putchar(a), ++cnt
   va_list arg;
   va_start(arg, frm);
-  // for (int i = 0; i < strlen(frm); i++) {
   // 字符串结尾标识符 \0, ASCII 码就是 0
   // strlen 就是循环字符串到 \0
   for (int i = 0; frm[i]; i++) {
@@ -18,28 +40,32 @@ int my_printf(const char *frm, ...) {
       case '%': {
         switch (frm[++i]) {
           case 'd': {
-            int x = va_arg(arg, int), temp = 0, digit = 0;
+            // INT32_MAX = 2147483647 INT32_MIN = -2147483648
+            // 用 int 处理负数取反时会超界，所以用 uint32_t
+            int xx = va_arg(arg, int), temp = 0, digit = 0;
+            uint32_t x;
             // 处理负数
-            if (x < 0); PUTC('-'), x = -x;
-            // 因为下一个循环时从后往前输出数字字符，所以先颠倒一下
-            // 123 => 321, 但 1000 => 0001 => 1
-            // 如果用 while，x 是 0 的时候无法进入，所以用 do while
-            do {
-              temp = temp * 10 + x % 10;
-              x /= 10;
-              // 记录位数，如果还有位数，但没有数字，就补 0
-              digit += 1;
-            } while (x);
-            // 反着从后往前输出数字字符
-            while (digit--) {
-              // convert int to char through + '0'.
-              // The char data type is represented as ascii values in c programming
-              PUTC(temp % 10 + '0');
-              temp /= 10;
-            }
+            if (xx < 0) PUTC('-'), x = -xx;
+            x = xx;
+            // 输出 INT32_MAX (2147483647) 时，反转数字是可能会超过这个数字，就超界了，所以分两段处理
+            int num1 = x / 100000, num2 = x % 100000;
+            int temp1 = 0, temp2 = 0;
+            // 因为输出时从后往前输出数字字符，所以先颠倒一下
+            int digit1 = reverse_num(num1, &temp1);
+            int digit2 = reverse_num(num2, &temp2);
+            if (num1) digit2 = 5;
+            else digit1 = 0;
+            cnt += output_num(temp1, digit1);
+            cnt += output_num(temp2, digit2);
           } break;
           case '%': {
             PUTC(frm[i]);
+          } break;
+          case 's': {
+            const char *str = va_arg(arg, const char *);
+            for (int i = 0; str[i]; i++) {
+              PUTC(str[i]);
+            }
           } break;
         }
       } break;
@@ -47,6 +73,7 @@ int my_printf(const char *frm, ...) {
     }
   }
   va_end(arg);
+  #undef PUTC
 	return cnt;
 }
 
@@ -57,9 +84,16 @@ int main() {
   int a = -123;
 	printf("hello world\n");
 	my_printf("hello world\n");
-  printf("int (a) = %%\n");
-  my_printf("int (a) = %%\n", a);
+  // printf("int (a) = %%\n");
+  // my_printf("int (a) = %%\n", a);
   printf("int (a) = %d\n", a);
   my_printf("int (a) = %d\n", a);
+  printf("INT32_MAX = %d\n", INT32_MAX);
+  my_printf("INT32_MAX = %d\n", INT32_MAX);
+  printf("INT32_MIN = %d\n", INT32_MIN);
+  my_printf("INT32_MIN = %d\n", INT32_MIN);
+  char str[100] = "I love Beijing !";
+  printf("%s\n", str);
+  my_printf("%s\n", str);
 	return 0;
 }
