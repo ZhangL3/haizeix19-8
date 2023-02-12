f4710f08062d38bde3cd7770acd9b8a07d45bd04
L3 Zhang <zhanglebielefeld@gmail.com>
2023-02-12 11:42:31 +0100

    OJ191

--- 01programDesignInC/05arrayString/OJ191.cpp
+++ 01programDesignInC/05arrayString/OJ191.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <inttypes.h>
 
 void markNotPrimeNums(bool *notPrimeNums, int size) {
 	for (int i = 2; i * i < size; i++) {
@@ -10,18 +11,38 @@ void markNotPrimeNums(bool *notPrimeNums, int size) {
 }
 
 int main() {
-	int l, r, n, maxDist = 0, lastPrime = 2;
+	int l, r;
+	int lFarPrime = 0, rFarPrime = 0, lClosePrime = 0, rClosePrime = 0;
+	int closestDist, farstDist;
+	int lastPrime = 0, minDist = INT8_MAX, maxDist = INT8_MIN, curDist;
 	bool notPrimeNums[8000005];
-	scanf("%d", &n);
-	markNotPrimeNums(notPrimeNums, n);
-	for (int i = 0; i < 20; i++) {
-		printf("%d: %d\n", i, notPrimeNums[i]);
+	scanf("%d %d", &l, &r);
+	markNotPrimeNums(notPrimeNums, r + 1);
+	for (int i = l; i <= r; i++) {
+		// printf("%d: %d\n", i, notPrimeNums[i]);
+		// if i is prime
 		if (!notPrimeNums[i]) {
-			maxDist = i - lastPrime > maxDist ? (i - lastPrime) : maxDist;
+			if (lastPrime) {
+				curDist = i - lastPrime;
+				if (curDist < minDist) {
+					minDist = curDist;
+					lClosePrime = lastPrime;
+					rClosePrime = i;
+				}
+				if (curDist > maxDist) {
+					maxDist = curDist;
+					lFarPrime = lastPrime;
+					rFarPrime = i;
+				}
+			}
 			lastPrime = i;
 		}
 	}
-	printf("%d\n", maxDist);
+	if (minDist != INT8_MAX && maxDist != INT8_MIN) {
+		printf("%d,%d are closest, %d,%d are most distant.\n", lClosePrime, rClosePrime, lFarPrime, rFarPrime);
+	} else {
+		printf("There are no adjacent primes.");
+	}
 
 	return 0;
 }
