68beac52aa17704caefbd8377bb407869640c883
L3 <l3@gmail.com>
2023-02-16 08:22:01 +0100

    first test framework demo, not ok

--- 01programDesignInC/08testFramework/ownTest/haizei/test.h
+++ 01programDesignInC/08testFramework/ownTest/haizei/test.h
@@ -3,10 +3,32 @@
 
 // 函数名不能写死了，因为可能有多个测试用例 TEST，函数名不能重复
 // ## 用于粘连两个传入的符号
-#define TEST(a, b) void a##b()
+// 带有 constructor 属性的函数，会先于主函数去执行
+#define TEST(a, b) \
+void a##_haizei_##b(); \
+__attribute__((constructor)) \
+// 这个函数会被先于主函数执行
+// 添加函数指针到 list，让 RUN_ALL_TESTS 知道都有哪些测试用例
+void add##_haizei_##a##_haizei_##b() { \
+  add_function(a##_haizei_##b, #a "_haize_" #b); \
+  // add_function(Name(a, b), Str(Name(a, b)));
+} \
+void a##_haizei__##b()
+
 // # 替换传入的符号
 #define EXPECT_EQ(a, b) printf("%s == %s ? %s\n", #a, #b, (a) == (b) ? "True" : "False" );
 
+typedef void (*TestFuncT)();
+
+struct Function {
+  TestFuncT func;
+  const char *str;
+};
+
 int RUN_ALL_TESTS();
+// 第一个参数是函数指针
+// 第二个参数是字符串，函数名
+void add_function(TestFuncT, const char *);
+
 
 #endif
--- 01programDesignInC/08testFramework/ownTest/main.cpp
+++ 01programDesignInC/08testFramework/ownTest/main.cpp
@@ -6,7 +6,7 @@ int add(int a, int b) {
 	return a + b;
 }
 
-TEST(testFunc, add1) {
+TEST(testFunc, add) {
 	EXPECT_EQ(add(5, 3), 8);
 	// EXPECT 遇到错误不结束
 	EXPECT_EQ(add(5, 3), 9);
@@ -24,7 +24,18 @@ TEST(testFunc, add2) {
 	EXPECT_EQ(add(6, 7), 15);
 }
 
+// 会有重定义的问题，可以在 TEST 宏定义里加特殊字符，如 _haizei_
+TEST(test, Funcadd) {
+	EXPECT_EQ(add(5, 3), 8);
+	// EXPECT 遇到错误不结束
+	EXPECT_EQ(add(5, 3), 9);
+	// ASSERT 遇到错误强行结束
+	// ASSERT_EQ(add(5, 3), 9);
+	EXPECT_EQ(add(6, 7), 15);
+}
+
 
 int main(int argc, char *argv[]) {
+  // RUN_ALL_TESTS 怎么知道有多少个测试用例: 函数指针
 	return RUN_ALL_TESTS();
 }
