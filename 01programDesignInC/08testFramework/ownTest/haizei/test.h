6eb49b146e2309200666f317526106695e8a9a13
L3 Zhang <zhanglebielefeld@gmail.com>
2023-02-18 17:34:13 +0100

    Add comment

--- 01programDesignInC/08testFramework/ownTest/haizei/test.h
+++ 01programDesignInC/08testFramework/ownTest/haizei/test.h
@@ -2,13 +2,15 @@
 #define _TEST_H
 
 // 函数名不能写死了，因为可能有多个测试用例 TEST，函数名不能重复
-// ## 用于粘连两个传入的符号
+// ## 用于粘连两个传入的符号，不做替换
 // 带有 constructor 属性的函数，会先于主函数去执行
 
 // 这个函数 (add) 会被先于主函数执行, 因为被标记为 constructor
 // 添加函数指针到 list，让 RUN_ALL_TESTS 知道都有哪些测试用例
 
-// add_function(Name(a, b), Str(Name(a, b))); 尝试改写
+// add_function(Name(a, b), Str(Name(a, b))); 尝试改写, Name 和 Str 是两个宏
+
+// void a##_haizei_##b() 后面是 TEST(a, b) 后的大括号
 #define TEST(a, b) \
 void a##_haizei_##b(); \
 __attribute__((constructor)) \
