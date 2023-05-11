d128302f151014dd15c13ca6fd6d59516496123a
Le Zhang <zhangl@duerkopp-adler.com>
2023-05-11 08:11:16 +0200

    understand getopt

--- 05OS/01fileAndDir/getoptExp.cpp
+++ 05OS/01fileAndDir/getoptExp.cpp
@@ -1,34 +1,12 @@
-# 操作系统与系统编程
-
-## 文件与目录操作
-
-### 相关命令实现
-
-#### 使用 getopt 解析命令行参数
-
-- 参数(arguments)和选项(option)的区别
-	- ls -a /etc
-		- -a 选项
-			- a 选项字符 (option character)
-		- /etc 参数
-- man 3 getopt
-
-```
-// 参数和 main 函数相同
-int getopt(int argc, char * const argv[],
-				const char *optstring);
-
-// 全局变量
-extern char *optarg;
-extern int optind, opterr, optopt;
-
-// example
-
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
+	printf("argc = %d\n", argc);
+	for (int i = 0; i < argc; i++) {
+		printf("argv[%d] = %s\n", i, argv[i]);
+	}
 	int flags, opt;
 	int nsecs, tfnd;
 
@@ -39,17 +17,30 @@ int main(int argc, char *argv[]) {
 	// optstring: "nt:", n 选项没有选项参数, t 选项有选项参数
 	while ((opt = getopt(argc, argv, "nt:")) != -1) {
 		// optind shows the index of next element to be processed in argv array
+		printf("opt = %d\n", opt);
+		// optopt 储存第一个错误选项字符,并返回给stderr
+		printf("optopt = %d\n", optopt);
+		// 全局变量 opterr 可用来禁止显示由 getopt() 打印出的错误消息。默认情况下,这个变量被设为1。如果我们将它设为0,那么 getopt() 将不再打印错误消息,而是表现的如同 上一条所描述的那样。程序可以通过检查函数返回值是否为?字符来判断是否出错, 并打印出用户自定义的错误消息。
+		printf("opterr = %d\n", opterr);
+		printf("optind = %d in while\n", optind);
+		printf("optarg = %s in while\n", optarg);
 		switch (opt) {
 		case 'n':
 			flags = 1;
+			printf("optind = %d by n\n", optind);
+			printf("optarg = %s by n\n", optarg);
 			break;
 		case 't':
 			// atoi: 字符串专成整形
+			printf("optind = %d by t\n", optind);
+			printf("optarg = %s by t\n", optarg);
 			nsecs = atoi(optarg);
 			tfnd = 1;
 			break;
 		default: /* '?' */
 			// fprintf: 打印到一个文件 (stderr)
+			printf("optind = %d by default\n", optind);
+			printf("optarg = %s by default\n", optarg);
 			fprintf(stderr, "Usage: %s [-t nsecs] [-n] name\n",
 					// argv[0]: 程序自己
 					argv[0]);
@@ -62,6 +53,7 @@ int main(int argc, char *argv[]) {
 			flags, tfnd, nsecs, optind);
 
 	// 索引值大于参数数量，就是没跟参数
+	printf("optind = %d after while loop\n", optind);
 	if (optind >= argc) {
 		fprintf(stderr, "Expected argument after options\n");
 		exit(EXIT_FAILURE);
@@ -74,6 +66,3 @@ int main(int argc, char *argv[]) {
 
 	exit(EXIT_SUCCESS);
 }
-
-```
-
