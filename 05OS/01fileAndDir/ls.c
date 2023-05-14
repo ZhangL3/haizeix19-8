6afbcd674e63075ee59ea9327a522dc514394171
L3 Zhang <zhanglebielefeld@gmail.com>
2023-05-14 20:36:38 +0200

    ls_do get params

--- 05OS/01fileAndDir/ls.c
+++ 05OS/01fileAndDir/ls.c
@@ -5,6 +5,10 @@
 int flag_a = 0;
 int flag_l = 0;
 
+void do_ls(char *dirname) {
+  printf("Doing with dir %s.\n", dirname);
+}
+
 int main(int argc, char **argv) {
   int opt;
   while((opt = getopt(argc, argv, "al")) != -1) {
@@ -22,6 +26,26 @@ int main(int argc, char **argv) {
   }
 
   printf("flag_a = %d, flag_l = %d\n", flag_a, flag_l);
+
+  // argc 更新为 getopt 解析完后，剩下参数的个数
+  // optind 指向下一个要解析的位置，是处理完选项后的下一个位置，所以得 -1
+  printf("optind = %d\n", optind);
+  argc -= (optind - 1);
+  // 指针向后移动到最后一个选项的位置
+  argv += (optind - 1);
+
+  // argv + 1 是第一个参数的位置
+  // argc 是 函数调用(1)和参数个数的和, 对应的参数是 *(argv + 1)
+  printf("argc = %d, argv=%s\n", argc, *(argv + 1));
+
+  // 1 表示只有函数调用，没有参数
+  if (argc == 1){
+    do_ls(".");
+  } else {
+    while(--argc) {
+      do_ls(*(++argv));
+    }
+  }
 
   return 0;
 }
