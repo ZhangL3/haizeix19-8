c450c1f34bbc1085c4c82fb194a0fa5f08f5536c
Le Zhang <zhangl@duerkopp-adler.com>
2023-05-30 08:37:16 +0200

    get cmd from /dev/tty

--- 05OS/01fileAndDir/more.c
+++ 05OS/01fileAndDir/more.c
@@ -10,7 +10,8 @@ int main(int argc, char **argv) {
 	// 文件指针，文件流
 	FILE *fp;
 	if (argc == 1) {
-		printf("Usage: %s [filename | stdin]\n", argv[0]);
+		// printf("Usage: %s [filename | stdin]\n", argv[0]);
+		do_more(stdin);
 	} else {
 		while (--argc) {
 			if ((fp = fopen(*++argv, "r")) != NULL ) {
@@ -24,12 +25,14 @@ int main(int argc, char **argv) {
 
 void do_more(FILE *fp) {
 	char line[LENLINE] = {0};
-	int num_line = 0, reply, get_cmd();
+	// 从终端中打开流
+	FILE *cmd = fopen("/dev/tty", "r");
+	int num_line = 0, reply, get_cmd(FILE*);
 	// 一行一行的读入流里的字符串到 line 中
 	while (fgets(line, LENLINE, fp)) {
 		// 如果到了一页末尾，等待用户输入
 		if (num_line == PAGELINE) {
-			reply = get_cmd();
+			reply = get_cmd(cmd);
 			if (reply == 0) break;
 			// 执行完输入命令减掉对应行数，可时间继续打印
 			num_line -= reply;
@@ -45,11 +48,12 @@ void do_more(FILE *fp) {
 	}
 }
 
-int get_cmd() {
+// 如果没有明确输入设备，./a.lout < 1.log 时， 1.log 里的字符也会作为输入控制传给 get_cmd
+int get_cmd(FILE *fp) {
 	printf("more:");
 	int c;
 	// getchar: 读取输入
-	while ((c = getchar()) != EOF) {
+	while ((c = fgetc(fp)) != EOF) {
 		// 退出
 		if (c == 'q') return 0;
 		// 输出下一页
