ce5ef05d52fda124d4d82a35175e7d7ccb879321
Le Zhang <zhangl@duerkopp-adler.com>
2023-06-26 08:03:50 +0200

    advanced IO

---
+++ 05OS/02blockedAndUnblockedIO/unblockIO_uncomplete/client.c
@@ -0,0 +1,23 @@
+#include "head.h"
+#include "tcp_client.h"
+#include "common.h"
+
+int main(int argc, char **argv) {
+  char msg[512] = {0};
+  int sockfd;
+  if (argc != 3) {
+    fprintf(stderr, "Usage: %s ip port\n", argv[0]);
+    return 1;
+  }
+
+  if ((sockfd = socket_connect(argv[1], atoi(argv[2]))) < 0) {
+    perror("socket_connect");
+    return 2;
+  }
+  make_nonblock_ioctl(sockfd);
+  recv(sockfd, msg, 512 0);
+  printf("rec: %s\n", msg);
+  close(fd); // not sockfd?
+
+  return 0;
+}
