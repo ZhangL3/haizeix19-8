f3b863e050ac50d07287ef019e7db2106e20fcd1
Le Zhang <zhangl@duerkopp-adler.com>
2023-07-26 08:11:12 +0200

    understand select

--- 05OS/C01fileAndDir/04IOmultiplexing/selectExp.c
+++ 05OS/C01fileAndDir/04IOmultiplexing/selectExp.c
@@ -2,8 +2,7 @@
 #include <stdlib.h>
 #include <sys/select.h>
 
-int
-main(void)
+int main(void)
 {
 	fd_set rfds;
 	struct timeval tv;
@@ -14,6 +13,7 @@ main(void)
 	// clear rfds
 	FD_ZERO(&rfds);
 	// add df: 0 to rfds
+	// 0: stdin
 	FD_SET(0, &rfds);
 
 	/* Wait up to five seconds. */
@@ -21,16 +21,20 @@ main(void)
 	tv.tv_sec = 2;
 	tv.tv_usec = 0;
 
+	// 1: 最大文件描述符 + 1
 	retval = select(1, &rfds, NULL, NULL, &tv);
 	/* Don't rely on the value of tv now! */
-	printf("FD_ISSET(0, &rfds) before getting anything: %d\n", FD_ISSET(0, &rfds));
+	printf("tv finished\n");
 
 	if (retval == -1)
 		perror("select()");
 	else if (retval) {
+		char buff[512] = {0};
 		printf("Data is available now.\n");
 		/* FD_ISSET(0, &rfds) will be true. */
 		printf("FD_ISSET(0, &rfds) after getting data: %d\n", FD_ISSET(0, &rfds));
+		// 标准输出被读入 buff 后，就不会被之后的 bash 接收了
+		// scanf("%s", buff);
 	}
 	else {
 		printf("FD_ISSET(0, &rfds) after expiring: %d\n", FD_ISSET(0, &rfds));
